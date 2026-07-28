071012e5d895fbee4e991754eefd3e9e2d6988f8
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-28 09:39:54 +0200

    Begun ex. 1

--- chapter-5/exercises.js
+++ chapter-5/exercises.js
@@ -1,11 +1,11 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-let arr = arrays.concat(arrays[0], arrays[1], arrays[2]);
+let arr = arrays.reduce(putTogether);
 
 function putTogether(arr){
     let array;
     for(let element of arr){
-        array.push(element);
+        ;
     }
     return array;
 }
