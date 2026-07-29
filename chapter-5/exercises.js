30bd76ad90ca2d9334ebade07a00638a4eb84856
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-29 08:33:11 +0200

    Solved ex. 2

--- chapter-5/exercises.js
+++ chapter-5/exercises.js
@@ -1,13 +1,20 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-console.log(arrays.reduce((flat, current) => flat.concat(current, [])));
+console.log(arrays.reduce((acc, current) => acc.concat(current, [])));
 
 
 // → [1, 2, 3, 4, 5, 6]
 
 
 // Your code here.
+function loop(n, test, update, body){
+    while(test(n)){
+        body(n);
+        n = update(n);
+    }
 
+    return body("Failed");
+}
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
