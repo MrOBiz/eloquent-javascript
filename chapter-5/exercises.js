30bd736e38441ce1ce7d5c09b6006a9cd5aec7a5
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-29 08:54:11 +0200

    Solved ex. 3

--- chapter-5/exercises.js
+++ chapter-5/exercises.js
@@ -6,6 +6,10 @@ console.log(arrays.reduce((acc, current) => acc.concat(current, [])));
 // → [1, 2, 3, 4, 5, 6]
 
 
+
+
+
+
 // Your code here.
 function loop(n, test, update, body){
     while(test(n)){
@@ -20,3 +24,44 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
+
+
+
+
+
+
+
+function every(array, test) {
+  // Your code here.
+    let n = 0;                 
+    for(let element of array){
+            if(test(element)){
+                n += 1
+            }
+    }
+
+    return (n === array.length)? true : false; 
+
+    //return !array.some(element => !test(element)); NOT RECOGNIZED
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+
+
+
+
+
+function dominantDirection(text) {
+  // Your code here.
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
