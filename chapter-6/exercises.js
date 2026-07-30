3f87cf7a206e626fa6d35df23b4dbcbcd829f21c
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-30 10:54:53 +0200

    Begun ex.2

--- chapter-6/exercises.js
+++ chapter-6/exercises.js
@@ -25,3 +25,47 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+
+
+
+
+
+class Group {
+  // Your code here.
+  constructor (...args){
+    args.forEach((value, index) => {
+        this["val" + (index +1)]
+    });
+  }
+
+  add(){
+
+  }
+
+  delete(){
+
+  }
+
+  has(){
+
+  }
+
+  static from (obj){
+    let grp = new Group;
+        for(let elt of obj){
+            
+        }
+    return grp;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
