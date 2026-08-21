824f69d09d513e7abb4b7575d9ee919cf4318d6c
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-08-21 17:29:35 +0200

    Added chapter-4 ex.1 first points

---
+++ chapter-4/exercises.js
@@ -0,0 +1,32 @@
+console.log("EX 1");
+
+function range(start, end, step){
+    let arr = [];
+    //if(start < end){
+        for(let i = start; i <= end; i + step){
+            arr.push(i);
+        }
+    /* }else if(start > end){
+        for(let i = end; i >= start; i - step){
+            arr.push(i);
+        }
+    } */
+
+    return arr;
+}
+
+function sum(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
