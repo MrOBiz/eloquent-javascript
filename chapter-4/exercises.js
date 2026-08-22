ddf13240d6041f960ff5ece263d1ba2710c78f26
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-08-22 10:42:01 +0200

    Solved ex. 1 with perfect adherency to output

--- chapter-4/exercises.js
+++ chapter-4/exercises.js
@@ -97,3 +97,27 @@ console.log(arrayValue);
 
 
 console.log("EX 3");
+
+
+let arrayToList = (arr) => {
+    let list = {};    
+    if(arr.length != 0){
+        list = {value: arr[0], 
+                rest: arrayToList(arr.slice(1)) };
+        return list;
+    }else{
+        return null;
+    }
+}
+    
+
+
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+//console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+//console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+//console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
