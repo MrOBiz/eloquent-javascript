9f50855747b137e18c4dd9fe60c58c35990d29da
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-08-22 11:44:51 +0200

    Started listToArray()

--- chapter-4/exercises.js
+++ chapter-4/exercises.js
@@ -110,12 +110,18 @@ let arrayToList = (arr) => {
     }
 }
     
-
+function listToArray(list){
+    let arr = [];
+    {
+        arr.push();
+    }
+    return arr;
+}
 
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 //console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
