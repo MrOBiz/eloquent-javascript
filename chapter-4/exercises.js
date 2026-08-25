2fcfd8fe4ace3ecf6da1b913e11c778d30cbefec
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-08-25 09:02:21 +0200

    Added book solution

--- chapter-4/exercises.js
+++ chapter-4/exercises.js
@@ -219,6 +219,25 @@ function deepEqual(a, b){
 }
 
 
+//BOOK SOLUTION
+/* function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+} */
+
+
 console.log(deepEqual(null, null));
 console.log(deepEqual(null, {}));
 console.log(deepEqual(1, "1"));
