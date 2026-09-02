a16266fd6511fbddbb5dfe52567fc2fbb92a69a2
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-09-02 16:25:25 +0200

    Added constructor function and EX 2 stub

--- chapter-6/exercises.js
+++ chapter-6/exercises.js
@@ -24,8 +24,20 @@ class Vec {
         let distance = 0;
         return distance = Math.sqrt(this.x**2 + this.y**2);
     }
+} 
+
+
+//common function version
+function makeVec(x, y){
+    let vec = {};
+    vec.x = x;
+    vec.y = y;
+
+    return vec;
 }
 
+//BOOK SOLUTION
+//same as my class
 
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
@@ -33,3 +45,30 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+console.log(makeVec(1, 2));
+
+
+
+
+
+
+
+
+
+
+console.log("EX 2");
+
+class Group {
+  // Your code here.
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
